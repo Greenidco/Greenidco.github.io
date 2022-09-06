cf927b5807c59a58af80b8c2587ad1ec4a7d9ab8
Greenidco <108576798+Greenidco@users.noreply.github.com>
2022-09-06 12:11:53 +0430

    Add files via upload

---
+++ wpscripts/nav_171tree.js
@@ -0,0 +1,250 @@
+var nav_171tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Cooler',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/cooler.html',
+'sTarget':'_self',"childArray" : [
+{   'bSeparatorBefore':true,
+'bSeparatorAfter':true,
+'sTitle':'Grate Cooler',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/greenid grate cooler.html',
+'sTarget':'_self',"childArray" : [
+{   'bSeparatorAfter':true,
+'sTitle':'Hammer Crusher',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Hammer crusher.html',
+'sTarget':'_self'
+},
+{   'sTitle':'hydraulic Drives ',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/hydraulic drives .html',
+'sTarget':'_self'
+}]
+},
+{   'sDescription':'white cement Submerged Chain Conveyor Submerged Scraper Chain',
+'sTitle':'Submerged Scraper Chain',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Submerged Scraper Chain.html',
+'sTarget':'_self'
+}]
+},
+{   'sTitle':'Kiln',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/kiln.html',
+'sTarget':'_self',"childArray" : [
+{   'bSeparatorAfter':true,
+'sTitle':'Rotary Kiln',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Rotary Kiln.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Kiln Support Roller',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Kiln Support roller.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Kiln Sealing',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Kiln Sealing.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Kiln Drive',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Kiln Drive.html',
+'sTarget':'_self'
+}]
+}]
+},
+{   'sTitle':'Silo',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Bulk Silo.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'High Capacity Silo',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/High Capacity Silo.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Conventional Cone Silo',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/conventional Cone Silo.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Steel Silo',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Steel Silo.html',
+'sTarget':'_self'
+}]
+},
+{   'sTitle':'Material Handling',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Handling system.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Reclaimer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Reclaimer.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Belt Conveyor',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/greenid belt conveyor.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Bucket Elevator',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Bucket Elevator.html',
+'sTarget':'_self'
+},
+{   'bSeparatorAfter':true,
+'sTitle':'Pneumatic Conveyor',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/greenid Pneumatic Conveyor.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Screw Conveyor',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Screw Conveyor.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Apron Conveyor',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Apron Conveyor.html',
+'sTarget':'_self'
+}]
+},
+{   'sTitle':'Air cleaning',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Air cleaning.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'horizontal Scrubber',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/horizontal scrubber.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Vertical Scrubber',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/vertical scrubber.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Dust Collector',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Dust Collector.html',
+'sTarget':'_self'
+},
+{   'bSeparatorAfter':true,
+'sTitle':'Separator Classifier',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Separator classifire .html',
+'sTarget':'_self'
+}]
+},
+{   'sTitle':'Silencer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Silencer.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Fan Silencer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Fan & Silencer.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Steam Silencer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/steam silencer.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Compressor Silencer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Compressor Silencer.html',
+'sTarget':'_self'
+},
+{   'sDescription':' gas turbines silencer, bypass silencer,  HRSG ducts silencer, main stacks silencer, ',
+'sTitle':'Flue Gas Silencer',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Flue gas Silencer.html',
+'sTarget':'_self'
+}]
+},
+{   'bSeparatorAfter':true,
+'sTitle':'General',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/General Engineering.html',
+'sTarget':'_self',"childArray" : [
+{   'bSeparatorBefore':true,
+'bSeparatorAfter':true,
+'sTitle':'سوخت رسانی',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Utility.html',
+'sTarget':'_self'
+},
+{   'sTitle':'داکت',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Duct.html',
+'sTarget':'_self'
+},
+{   'sTitle':'استراکچر فلزی',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Greenid Steel structure.html',
+'sTarget':'_self'
+}]
+},
+{   'bSeparatorAfter':true,
+'sTitle':'مهندسی معکوس',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Reverse engineering.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'الکترولیز',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/electrowinning.html',
+'sTarget':'_self'
+},
+{   'bSeparatorAfter':true,
+'sTitle':'سرند کوچک',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/small screen.html',
+'sTarget':'_self'
+},
+{   'sTitle':'الک تیغه ای',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/wedge screen.html',
+'sTarget':'_self'
+}]
+}]
+});
---
+++ wpscripts/nav_377tree.js
@@ -0,0 +1,49 @@
+var nav_377tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'./',
+'sTarget':'_self'
+},
+{   'sTitle':'Purposes',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/about us.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Download',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/Download.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Products',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/greenid product.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Education',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/education.html',
+'sTarget':'_self'
+},
+{   'bSeparatorBefore':true,
+'bSeparatorAfter':true,
+'sTitle':'Certificates',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/CERTIFICATES.html',
+'sTarget':'_self'
+},
+{   'bSeparatorBefore':true,
+'bSeparatorAfter':true,
+'sTitle':'Contact Us',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'wpscripts/contact us.html',
+'sTarget':'_self'
+}]
+});
